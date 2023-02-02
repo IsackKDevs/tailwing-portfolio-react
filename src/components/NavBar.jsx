d00cdc61392c02cdbf63291d27af764dd504d162
isack kipanga <isack.kipanga@gmail.com>
2023-02-02 15:28:33 -0500

    int

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FaBars, FaTimes} from "react-icons/fa";
+
+const NavBar = () => {
+  return (
+    <div className="flex justify-between items-center h-20
+    px-4 text-white bg-black fixed ">
+        <div>
+            <h1>zak</h1>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
